8eab8e59c5f3f791e7575a6525892641540e858c
Sammus-s <samuel.rodrigues2004@hotmail.com>
2025-03-27 20:55:10 -0300

    created soundHandler class

--- audio_handler.py
+++ audio_handler.py
@@ -1,7 +1,19 @@
-class AudioHandler:
-    pygame.mixer.init()
-    back_ground_music = pygame.mixer.music
-    back_ground_music.load("Alarm01.wav")
-    back_ground_music.play(-1)
+import pygame
 
-    sound_effect = pygame.mixer.Sound("walkieTalkieStart.mp3")
+class AudioHandler:
+    """Base class for handling audio in the game."""
+    def __init__(self):
+        pygame.mixer.init()
+
+
+class SoundHandler(AudioHandler):
+    """Handles sound effects."""
+    def __init__(self, sound_path):
+        super().__init__()
+        self.sound = pygame.mixer.Sound(sound_path)
+
+    def play_bounce(self):
+        """Plays the bounce sound effect."""
+        self.sound.play()
+
+
