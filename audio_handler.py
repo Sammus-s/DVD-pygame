dbfee79cd0dabbe873b9fbdbe9422c44158048e2
Sammus-s <samuel.rodrigues2004@hotmail.com>
2025-03-27 20:55:48 -0300

    created musicHandler class

--- audio_handler.py
+++ audio_handler.py
@@ -17,3 +17,24 @@ class SoundHandler(AudioHandler):
         self.sound.play()
 
 
+class MusicHandler(AudioHandler):
+    """Handles background music."""
+    def __init__(self, music: str):
+        super().__init__()
+        self.background_music = pygame.mixer.music
+        self.background_music.load(background_music)
+        self.background_music.play(-1)
+
+    def change_music(self, new_music):
+        """Stops current playing music, and starts a new one"""
+        self.background_music.fadeout(1000)
+        self.background_music.unload()
+        self.background_music.load(new_music)
+        self.background_music.play(-1)
+
+    def toggle_music(self):
+        """Plays or Pauses background music."""
+        if not self.background_music.get_busy:
+            self.background_music.unpause()
+        else:
+            self.background_music.pause()
