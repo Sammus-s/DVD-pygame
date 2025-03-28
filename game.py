b1a904938eeeded9a5ba6ffdb0aa085902e1b130
Sam <samuel.rodrigues2004@hotmail.com>
2025-03-27 23:51:24 -0300

    docstring on game.py

--- game.py
+++ game.py
@@ -2,13 +2,15 @@ import sys
 import pygame
 from pathlib import Path
 
-
 from text_module import VerticalMoveText, HorizontalMoveText, BounceText
 from audio_module import MusicPlaylist
-from config import SCREEN_WIDTH,SCREEN_HEIGHT,BLACK,WHITE,FPS,BACKGROUND_MUSIC_PATH
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, WHITE, FPS, BACKGROUND_MUSIC_PATH
 
 class Game:
+    """Main class for the DVD simulation game."""
+
     def __init__(self):
+        """Initializes the game, setting up the screen, clock, and background music."""
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('DVD')
@@ -16,18 +18,28 @@ class Game:
         self.running = True
         self.text = BounceText("Samuel", 50, WHITE, './sounds/bounce.mp3', 5, 5)
 
-        self.background_music = MusicPlaylist([BACKGROUND_MUSIC_PATH+f.name for f in Path(BACKGROUND_MUSIC_PATH).iterdir()])
+        # Load background music playlist
+        self.background_music = MusicPlaylist(
+            [BACKGROUND_MUSIC_PATH + f.name for f in Path(BACKGROUND_MUSIC_PATH).iterdir()]
+        )
         self.background_music.music.play(-1)
-    
+
     def handle_events(self):
+        """Handles user input events, such as quitting and key presses."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             
             if event.type == pygame.KEYUP:
                 self.handle_key_press(event)
-    
+
     def handle_key_press(self, event):
+        """Handles key press events to control game actions.
+
+        - ESC: Quit the game.
+        - SPACE: Toggle background music (play/pause).
+        - S: Skip to the next track in the playlist.
+        """
         if event.key == pygame.K_ESCAPE:
             self.running = False
 
@@ -37,20 +49,24 @@ class Game:
         if event.key == pygame.K_s:
             self.background_music.next_music()
             self.background_music.music.play(-1)
-                        
+
     def update_text_position(self):
+        """Updates the position of the bouncing text."""
         self.text.update()
-    
+
     def draw_screen(self):
+        """Renders the screen, clears it, draws the text, and updates the display."""
         self.screen.fill(BLACK)
         self.text.draw(self.screen)
         pygame.display.flip()        
 
     def run(self):
+        """Main game loop. Handles events, updates the text, and redraws the screen."""
         while self.running:
             self.handle_events()
             self.update_text_position()
             self.draw_screen()
             self.clock.tick(FPS)
+        
         pygame.quit()
         sys.exit()
